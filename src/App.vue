34dd19128305506f56cd218c5ebace3aba4d2c91
JAY <1419456048@qq.com>
2019-05-30 14:53:37 +0800

    项目准备工作

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="app">
+    <h3>{{msg}}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      msg: "jay"
+    };
+  }
+};
+</script>
+
+<style >
+
+</style>
---
+++ src/components/test.vue
@@ -0,0 +1,26 @@
+
+<template>
+<div class="box">
+    <div class="son">
+
+    </div>
+</div>
+</template>
+<script>
+    export default {
+        name:'test',     
+    }
+</script>
+<style scoped lang="scss">
+    $color : pink;
+    .box{
+        width: 375px;
+        height: 100px;
+        background-color: $color;
+    }
+    .son{
+        width: 188px;
+        height: 100px;
+        background-color: green;
+    }
+</style>
